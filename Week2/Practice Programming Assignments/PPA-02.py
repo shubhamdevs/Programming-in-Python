3a4c1416118c6bf816b71fc2e4cbca6dd6ddc9d3
SHUBHAM BISWAS <suvm.dev@gmail.com>
2022-11-24 12:16:47 +0530

    add PPA-02, PPA-03, PPA-04

---
+++ Week2/Practice Programming Assignments/PPA-02.py
@@ -0,0 +1,8 @@
+x = float(input())
+
+if (0 < x < 10):
+    print(x + 2)
+elif (x >= 10):
+    print((x * x) + 2)
+else:
+    print(00.0)
---
+++ Week2/Practice Programming Assignments/PPA-03.py
@@ -0,0 +1,16 @@
+T = int(input())
+
+if T > 0:
+    if (0 <= T <=5):
+        print("NIGHT")
+    elif (T <= 11):
+        print("MORNING")
+    elif(T <= 17):
+        print("AFTERNOON")
+    elif (T <= 23):
+        print("EVENING")
+    else:
+        print("INVALID")
+else:
+    print("INVALID")
+        
