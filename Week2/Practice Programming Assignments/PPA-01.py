b8db14cb1ab32d5ce8bcdcbbb7ecc4f3097ccf92
SHUBHAM BISWAS <suvm.dev@gmail.com>
2022-11-24 11:13:54 +0530

    add PPA-01 in week2

---
+++ Week2/Practice Programming Assignments/PPA-01.py
@@ -0,0 +1,8 @@
+# Accept a non-zero integer as input. Print positive if it is greater than zero and negative if it is less than zero.
+
+num = int(input())
+
+if num > 0 :
+    print("positive")
+else:
+    print("negative")
