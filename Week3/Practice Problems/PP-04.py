92258640feeabb43ee192012b75db99a9a70cbfd
SHUBHAM BISWAS <suvm.dev@gmail.com>
2022-11-24 14:25:42 +0530

    add practice problem 4 in week 3

---
+++ Week3/Practice Problems/PP-04.py
@@ -0,0 +1,16 @@
+# Find whether the entered number is palindrome or not
+
+num = int(input("Enter a number: "))
+absNum = abs(num)
+rev = absNum % 10
+absNum = absNum // 10
+while(absNum > 0):
+    r = absNum % 10
+    absNum = absNum //10
+    rev = rev * 10 + r
+if (num < 0):
+    rev = rev - 2 * rev
+if (num == rev):
+    print("Palindrome")
+else:
+    print("Not a palindrome")
