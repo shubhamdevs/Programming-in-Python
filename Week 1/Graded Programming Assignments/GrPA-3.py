e1631e03d7d76716a65e7a9a363fa6aa5826da8c
SHUBHAM BISWAS <suvm.dev@gmail.com>
2023-01-01 23:39:58 +0530

    add spacing

--- Week 1/Graded Programming Assignments/GrPA-3.py
+++ Week 1/Graded Programming Assignments/GrPA-3.py
@@ -1,4 +1,6 @@
+
 # Problem: Accept a sequence of five single digit numbers separated by commas as input. Print the product of all five numbers.
+
 
 num = input()
 num1 = int(num[0])
@@ -6,4 +8,5 @@ num2 = int(num[2])
 num3 = int(num[4])
 num4 = int(num[6])
 num5 = int(num[8])
+
 print(num1 * num2 * num3 * num4 * num5)
