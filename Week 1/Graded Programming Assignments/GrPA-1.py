8c7fae2bbed38360179816fc50b39b099801ffde
SHUBHAM BISWAS <suvm.dev@gmail.com>
2023-01-01 23:41:16 +0530

    add spacing

--- Week 1/Graded Programming Assignments/GrPA-1.py
+++ Week 1/Graded Programming Assignments/GrPA-1.py
@@ -1,3 +1,4 @@
+
 # Problem: Accept five words as input and print the sentence formed by these words after adding a space between consecutive words and a full stop at the end.
 
 w1 = str(input())
@@ -5,4 +6,5 @@ w2 = str(input())
 w3 = str(input())
 w4 = str(input())
 w5 = str(input())
+
 print(w1 + " " + w2 + " " + w3 + " " + w4 + " " + w5 + ".")
