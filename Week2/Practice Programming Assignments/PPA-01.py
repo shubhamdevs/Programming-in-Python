c57297096e4a8d020da85f77fa9f4b38dfaef78d
SHUBHAM BISWAS <suvm.dev@gmail.com>
2023-01-01 23:43:51 +0530

    add spacing

--- Week2/Practice Programming Assignments/PPA-01.py
+++ Week2/Practice Programming Assignments/PPA-01.py
@@ -1,3 +1,4 @@
+
 # Accept a non-zero integer as input. Print positive if it is greater than zero and negative if it is less than zero.
 
 num = int(input())
@@ -6,3 +7,4 @@ if num > 0 :
     print("positive")
 else:
     print("negative")
+    
